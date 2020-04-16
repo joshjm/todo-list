6eca21bf99fd4351030be65a26885febe8db5988
Joshua McDonald <mcdojj12@gmail.com>
2020-04-16 23:57:57 +1000

    first commit

---
+++ todolist-v1/app.js
@@ -0,0 +1,14 @@
+//jshint esversion:6
+
+const express = require("express");
+const bodyParser = require("body-parser");
+
+const app = express();
+
+app.get("/", function(req, res){
+  res.sendFile(__dirname+"/index.html");
+});
+
+app.listen(3000, function(){
+  console.log("Server started on port 3000.");
+});
